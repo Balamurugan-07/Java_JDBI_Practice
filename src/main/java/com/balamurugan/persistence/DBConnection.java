6120040f46ae2eddd9189646f8565d9ca8be0f6f
Balamurugan D <balacricket2000@pec.edu>
2021-09-15 00:02:44 +0530

    Some changes

--- src/main/java/com/balamurugan/persistence/DBConnection.java
+++ src/main/java/com/balamurugan/persistence/DBConnection.java
@@ -3,10 +3,10 @@ package com.balamurugan.persistence;
 import org.skife.jdbi.v2.DBI;
 
 public class DBConnection {
-  private String url = "jdbc:mysql://localhost:3306/carsDemo";
+  private String url = "jdbc:mysql://localhost:3306/carsDemo?useSSL=false&allowPublicKeyRetrieval=true";
   private String uname = "root";
   private String pass = "bala";
-  public DBI connect() {
+  public final DBI connect() {
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
       DBI dbi = new DBI(url, uname, pass);
